b896817dde6c82e687d8862db4401954ae5cbc3c
Nusuno <nuskung2547@gmail.com>
2024-08-23 09:06:17 +0700

    Create index.ts

---
+++ EP.5/index.ts
@@ -0,0 +1,42 @@
+//ข้อสอบ กลางภาค ข้อสุดท้าย ปล. ผิดเละเทะมากกกก เศร้า+1
+// function toFixed(fName: string) {
+//     if (fName.includes('นาย')) {
+//         console.log(fName.slice(3,fName.length))
+//     } else if (fName.includes('นางสาว')) {
+//         console.log(fName.slice(6,fName.length))
+//     } else {
+//         console.log(fName)
+//     }
+// }
+
+// toFixed('นายทดสอบ')
+
+
+//for loop
+
+// for (let i = 0; i <= 5; i++) { //let i = 0; คือ เริ่มจากเลข 0 ใส่ไว้ใน i จบด้วย ; i <= 5; คือ ถ้า i น้อยกว่าหรือเท่ากับ 5 ให้มัน i++ คือ i บวกเพิ่มไปจนกว่าจะ === 5
+//     console.log(i)
+// }
+
+
+// let sum: number = 0 // สร้าง sum เพื่อรอรับเลขที่บวก
+
+//i++ = i บวกเพิ่มทีละ 1  
+// for (let i = 1; i <= 365; i++) { // สร้าง loop ในการบวกเลขไปเรื่อยๆ จนถึง 1000 ครั้ง 
+//     sum = sum + i // เอาตัวเองมาบวก 1
+// }
+
+
+// console.log('total : ' , sum)
+
+//while loop
+
+let i: number = 1
+let sum: number = 0 
+
+while (i <= 1000) {
+    sum = sum + i
+    i++
+}
+
+console.log(sum)
